b74d58271d390014d8a4b9f84e30c949a7334284
Anthony Ofume <praiseofume@gmail.com>
2018-12-19 17:48:43 +0100

    started testing with supertest. all codes written in the user.js file

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+const request = require('supertest');
+const app = require('../app');
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/contact_db');
@@ -83,5 +85,104 @@ router.delete('/', function (req, res, next) {
 router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
+
+
+
+
+
+/**
+ * TESTING TO GET ALL USERS ENDPOINT
+ */
+describe('GET /users', function () {
+  it('respond with json containing a list of all users', function (done) {
+    request(app)
+      .get('/users')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, done);
+  });
+});
+
+/**
+ * Testing get a user endpoint by giving an existing user
+ */
+describe('GET /user/:id', function () {
+  it('respond with json containing a single user', function (done) {
+    request(app)
+      .get('/users/U001')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, done);
+  });
+});
+
+/**
+* Testing get a user endpoint by giving a non-existing user
+*/
+describe('GET /user/:id', function () {
+  it('respond with json user not found', function (done) {
+    request(app)
+      .get('/users/idisnonexisting')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(404) //expecting HTTP status code
+      .expect('"user not found"') // expecting content value
+      .end((err) => {
+        if (err) return done(err);
+        done();
+      });
+  });
+});
+
+/**
+* Testing post user endpoint
+*/
+describe('POST /users', function () {
+  let data = {
+    "id": "1",
+    "name": "dummy",
+    "contact": "dummy",
+    "address": "dummy"
+  }
+  it('respond with 201 created', function (done) {
+    request(app)
+      .post('/users')
+      .send(data)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(201)
+      .end((err) => {
+        if (err) return done(err);
+        done();
+      });
+  });
+});
+
+/**
+* Testing post user endpoint
+*/
+describe('POST /users', function () {
+  let data = {
+    //no id
+    "name": "dummy",
+    "contact": "dummy",
+    "address": "dummy"
+  }
+  it('respond with 400 not created', function (done) {
+    request(app)
+      .post('/users')
+      .send(data)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(400)
+      .expect('"user not created"')
+      .end((err) => {
+        if (err) return done(err);
+        done();
+      });
+  });
+});
+
+
 
 module.exports = router;
